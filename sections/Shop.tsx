efa241e03c7edc27a0c139001fd55adef2efae69
Kojo Patrick <patrickasare84@gmail.com>
2022-05-10 05:20:00 +0000

    add shop section

--- sections/Shop.tsx
+++ sections/Shop.tsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import Image from "next/image";
+import Link from "next/link";
+import { shop } from "../utils/constants";
 
 const Shop = () => {
   return (
-    <div>Shop</div>
-  )
-}
+    <section className="section-shop" id="shop">
+      <div className="shop__image">
+        <Image
+          layout="fill"
+          objectFit="cover"
+          src={shop[0].img}
+          alt={shop[0].title + "Image"}
+        />
+      </div>
+      <div className="shop__content">
+        <h2 className="heading heading-2">{shop[0].title}</h2>
+        <p className="text-lg mt-lg-x">{shop[0].desc}</p>
+        <Link href="/">
+          <a className="btn shop__btn mt-lg-x">Let&apos;s Shop</a>
+        </Link>
+      </div>
+    </section>
+  );
+};
 
-export default Shop
+export default Shop;

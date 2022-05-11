e16ee76104d9258a659900e6e1788726b3ab04df
Kojo Patrick <patrickasare84@gmail.com>
2022-05-11 12:38:55 +0000

    clean ui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,12 @@
 //Styles
-import '../styles/globals.css'
+import "../styles/globals.css";
 import "@splidejs/splide/dist/css/splide.min.css";
-//import '@splidejs/splide/css/core';
 import "../styles/sass/main.scss";
-//import "@splidejs/react-splide/css";
 
-
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+    return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;

f1cb2fb21084795be1f3c21b381a4cc90be654a6
Kojo Patrick <patrickasare84@gmail.com>
2022-05-12 13:11:21 +0000

    [wip] sort ui

--- components/Sort.tsx
+++ components/Sort.tsx
@@ -1,26 +1,48 @@
-import { FiFilter } from "react-icons/fi";
+import { BsFillGrid3X3GapFill, BsListUl } from "react-icons/bs";
 
 const Sort = () => {
-    return    <>
-    <h3 className="heading heading-3 mt-lg-x search-results search-results__title">
-        Search
-    </h3>
-    <div className="result-description-with-filter">
-        <p className="text-lg-x search-results search-results__description">
-            Found 0 works
-        </p>
-
-        <div className="filter-container">
-            <span className="color-selected"></span>
-            <div className="color-swatch"></div>
+    return (
+        <div className="sort">
+            <div className="result-description-with-filter">
+                <div className="search-results__found">
+                    <p className="text-lg-x">Found :</p>
+                    <span>
+                        <button className="search-results__works btn btn-filter">
+                            0 works
+                        </button>
+                        <button className="search-results__sets btn btn-filter">
+                            0 sets
+                        </button>
+                    </span>
+                </div>
+                <div className="sort-results">
+                    <form action="">
+                        <label htmlFor="sort">Sort by</label>
+                        <select name="sort" id="sort" className="sort-input">
+                            <option value="relevance">Relevance</option>
+                            <option value="type-of-work">Type of Work</option>
+                            <option value="artist-a"> Artist (a-z)</option>
+                            <option value="artist-z">Artist (z-a)</option>
+                            <option value="objNum-a">
+                                Object number (a-z)
+                            </option>
+                            <option value="objNum-z">
+                                Object number (z-a)
+                            </option>
+                        </select>
+                    </form>
+                    <div className="btns-change-layouts">
+                        <button className="btn btn-filter">
+                            <BsFillGrid3X3GapFill className="icon icon-filter" />
+                        </button>
+                        <button className="btn btn-filter">
+                            <BsListUl className="icon icon-filter" />
+                        </button>
+                    </div>
+                </div>
+            </div>
         </div>
-
-        <button className="btn btn-filter">
-            <FiFilter className="icon icon-filter" />
-            Filter
-        </button>
-    </div>
-</>;
+    );
 };
 
 export default Sort;

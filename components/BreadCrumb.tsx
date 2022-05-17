8660813da879e64a7520454382a95cfa2aa64013
Kojo Patrick <patrickasare84@gmail.com>
2022-05-17 22:48:57 +0000

    update breadcrumb

--- components/BreadCrumb.tsx
+++ components/BreadCrumb.tsx
@@ -2,19 +2,27 @@ import Link from "next/link";
 
 interface BreadCrumbProps {
     title?: string;
-    id?: string;
+    id?: string | Number;
+    name?: string;
 }
 
-const BreadCrumb = ({ title, id }: BreadCrumbProps) => {
+const BreadCrumb = ({ title, id, name }: BreadCrumbProps) => {
     return (
         <section className="section-breadcrumb">
             <div className="section-center">
                 <h3>
                     <Link href="/">Home</Link>
-                    {title === "Museum" && (
-                        <Link href="/museum">/Collections</Link>
+                    {title === "Products" && (
+                        <Link href="/shop"> / Products</Link>
                     )}
-                    {id && <><Link href="/museum"> /Collections</Link> /{id}</>}
+                    {title === "Museum" && (
+                        <Link href="/museum"> /Collections</Link>
+                    )}
+                    {title === "collection" && (
+                        <Link href="/museum"> /Collections</Link>
+                    )}
+                    {id && <> / {id}</>}
+                    {name && <> / {name}</>}
                 </h3>
             </div>
         </section>
--- pages/shop/products/[id].tsx
+++ pages/shop/products/[id].tsx
@@ -1,5 +1,20 @@
+import { useRouter } from "next/router";
+import BreadCrumb from "../../../components/BreadCrumb";
+import Layout from "../../../components/Layout";
+
 const ProductDetails = () => {
-    return <div>ProductDetails</div>;
+    const router = useRouter();
+    const { id } = router.query;
+    return (
+        <Layout className="main-container">
+            <BreadCrumb title="Products" id={`${id}`} />
+            <div className="page-center">
+                <button className="shop-btn shop-btn__primary">
+                    Back to Products
+                </button>
+            </div>
+        </Layout>
+    );
 };
 
 export default ProductDetails;
